afdac649fb5ea8d1b715e2dabbb216a653d19089
Sophia Williams <sophiaashleighwilliams@gmail.com>
2024-01-31 10:49:24 -0500

    finalize homepage views

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,9 @@
 from app.routes import home
 from app.routes import home, dashboard
 from flask import Flask
+from app.db import init_db
+from app.utils import filters
+
 def create_app(test_config=None):
   # set up app config
   app = Flask(__name__, static_url_path='/')
@@ -10,10 +13,16 @@ def create_app(test_config=None):
   )
   @app.route('/hello') # 'decorator' creates route 
   def hello(): 
+    init_db(app)
     return 'hello world' # response 
 
   # register routes
   app.register_blueprint(home)
   app.register_blueprint(dashboard)
+
+  # register filters 
+  app.jinja_env.filters['format_url'] = filters.format_url
+  app.jinja_env.filters['format_date'] = filters.format_date
+  app.jinja_env.filters['format_plural'] = filters.format_plural
 
   return app
